c56e603d0af27e3e700af29443144503d556642d
Shantina <dinadamjanovik@gmail.com>
2019-04-21 12:22:04 +0200

    added

--- js/map.js
+++ js/map.js
@@ -2,7 +2,7 @@ $(function(){
     $('.mapLinks').click(function(e){
         e.preventDefault();
         $('.mapLinks').not($(this)).children('path').css({fill: 'white'});
-        $(this).children('path').css({fill: 'rgb(74, 151, 185)'});
+        $(this).children('path').css({fill: 'lightgrey'});
         console.log($(this).attr("id"));
     });
 });
